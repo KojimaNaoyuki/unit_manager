c1a142ecc66245960f8323d836905a38fe9eed95
KojimaNaoyuki <programkoji213@gmail.com>
2023-07-17 01:38:29 +0900

    add equation

--- lib/unit_manager/unit.rb
+++ lib/unit_manager/unit.rb
@@ -14,7 +14,43 @@ module UnitManager
 
     def derive_value(value)
       split_equation = @equation.split
-      
+
+      multiplication_index = split_equation.index('*')
+      division_index = split_equation.index('/')
+      addition_index = split_equation.index('+')
+      subtraction_index = split_equation.index('-')
+    end
+
+    def multiplication(split_equation:, index:)
+      result = split_equation[index-1] * split_equation[index+1]
+      split_equation.slice!(index, index+1)
+      split_equation[index-1] = result
+
+      split_equation
+    end
+
+    def division(split_equation:, index:)
+      result = split_equation[index-1] / split_equation[index+1]
+      split_equation.slice!(index, index+1)
+      split_equation[index-1] = result
+
+      split_equation
+    end
+
+    def addition(split_equation:, index:)
+      result = split_equation[index-1] + split_equation[index+1]
+      split_equation.slice!(index, index+1)
+      split_equation[index-1] = result
+
+      split_equation
+    end
+
+    def subtraction(split_equation:, index:)
+      result = split_equation[index-1] - split_equation[index+1]
+      split_equation.slice!(index, index+1)
+      split_equation[index-1] = result
+
+      split_equation
     end
   end
 end
