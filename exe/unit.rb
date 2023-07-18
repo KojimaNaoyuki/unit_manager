914e4975b2cb1d6a6b2929615bda4448d0eb4b05
KojimaNaoyuki <programkoji213@gmail.com>
2023-07-19 00:09:31 +0900

    fix exe

--- exe/unit.rb
+++ exe/unit.rb
@@ -1,9 +1,25 @@
 #!/usr/bin/env ruby
 
-Dir.mkdir('unit_config') unless Dir.exist?('unit_config')
-File.new('unit_config/unit.yml', 'w') unless File.exist?("unit_config/unit.yml")
+case ARGV[0]
+when 'init'
+  init
+when 'confirmation'
+  confirmation
+else
+  puts 'command does not exist'
+end
 
-ENV['unit_config_path'] = "#{Dir.pwd}/unit_config/unit.yml"
 
-puts('=== Initialize Exit ===')
-puts('- unit_config/unit.yml')
+def confirmation
+  puts "unit_config_path: #{ENV['unit_config_path']}"
+end
+
+def init
+  Dir.mkdir('unit_config') unless Dir.exist?('unit_config')
+  File.new('unit_config/unit.yml', 'w') unless File.exist?("unit_config/unit.yml")
+  
+  ENV['unit_config_path'] = "#{Dir.pwd}/unit_config/unit.yml"
+  
+  puts '=== Initialize Exit ==='
+  puts '- unit_config/unit.yml'
+end
