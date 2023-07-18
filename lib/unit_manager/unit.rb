10185cedecba1dbfae89ca0efbf9e31a1f0fbcac
KojimaNaoyuki <programkoji213@gmail.com>
2023-07-18 23:56:10 +0900

    split helper file

--- lib/unit_manager/unit.rb
+++ lib/unit_manager/unit.rb
@@ -1,6 +1,8 @@
 module UnitManager
   class Unit
     require 'active_support'
+    require_relative '../util/calculation_helper'
+    include CalculationHelper
 
     def initialize(key:, name:, equation:)
       @key = key
@@ -20,67 +22,6 @@ module UnitManager
       split_equation.map! { |array_value| array_value == "<value>" ? value.to_s : array_value }
 
       calculation_equation(split_equation: split_equation)
-    end
-    
-    def calculation_equation(split_equation:)
-
-      while split_equation.index('*').present? || split_equation.index('/').present? do
-        multiplication_index = split_equation.index('*').presence || 1000
-        division_index = split_equation.index('/').presence || 1000
-
-        if multiplication_index <= division_index
-          multiplication(split_equation: split_equation, index: multiplication_index)
-          next
-        end
-        
-        if division_index < multiplication_index
-          division(split_equation: split_equation, index: division_index)
-          next
-        end
-      end
-
-      while split_equation.index('+').present? || split_equation.index('-').present? do
-        addition_index = split_equation.index('+').presence || 1000
-        subtraction_index = split_equation.index('-').presence || 1000
-
-        if addition_index <= subtraction_index
-          addition(split_equation: split_equation, index: addition_index)
-          next
-        end
-        
-        if subtraction_index < addition_index
-          subtraction(split_equation: split_equation, index: subtraction_index)
-          next
-        end
-      end
-
-      result = split_equation[0]
-
-      result.to_f.round
-    end
-
-    def multiplication(split_equation:, index:)
-      result = split_equation[index-1].to_f * split_equation[index+1].to_f
-      split_equation.slice!(index, 2)
-      split_equation[index-1] = result.to_s
-    end
-
-    def division(split_equation:, index:)
-      result = split_equation[index-1].to_f / split_equation[index+1].to_f
-      split_equation.slice!(index, 2)
-      split_equation[index-1] = result.to_s
-    end
-
-    def addition(split_equation:, index:)
-      result = split_equation[index-1].to_f + split_equation[index+1].to_f
-      split_equation.slice!(index, 2)
-      split_equation[index-1] = result.to_s
-    end
-
-    def subtraction(split_equation:, index:)
-      result = split_equation[index-1].to_f - split_equation[index+1].to_f
-      split_equation.slice!(index, 2)
-      split_equation[index-1] = result.to_s
     end
   end
 end
---
+++ lib/util/calculation_helper.rb
@@ -0,0 +1,62 @@
+module CalculationHelper
+  def calculation_equation(split_equation:)
+  
+    while split_equation.index('*').present? || split_equation.index('/').present? do
+      multiplication_index = split_equation.index('*').presence || 1000
+      division_index = split_equation.index('/').presence || 1000
+  
+      if multiplication_index <= division_index
+        multiplication(split_equation: split_equation, index: multiplication_index)
+        next
+      end
+      
+      if division_index < multiplication_index
+        division(split_equation: split_equation, index: division_index)
+        next
+      end
+    end
+  
+    while split_equation.index('+').present? || split_equation.index('-').present? do
+      addition_index = split_equation.index('+').presence || 1000
+      subtraction_index = split_equation.index('-').presence || 1000
+  
+      if addition_index <= subtraction_index
+        addition(split_equation: split_equation, index: addition_index)
+        next
+      end
+      
+      if subtraction_index < addition_index
+        subtraction(split_equation: split_equation, index: subtraction_index)
+        next
+      end
+    end
+  
+    result = split_equation[0]
+  
+    result.to_f.round
+  end
+  
+  def multiplication(split_equation:, index:)
+    result = split_equation[index-1].to_f * split_equation[index+1].to_f
+    split_equation.slice!(index, 2)
+    split_equation[index-1] = result.to_s
+  end
+  
+  def division(split_equation:, index:)
+    result = split_equation[index-1].to_f / split_equation[index+1].to_f
+    split_equation.slice!(index, 2)
+    split_equation[index-1] = result.to_s
+  end
+  
+  def addition(split_equation:, index:)
+    result = split_equation[index-1].to_f + split_equation[index+1].to_f
+    split_equation.slice!(index, 2)
+    split_equation[index-1] = result.to_s
+  end
+  
+  def subtraction(split_equation:, index:)
+    result = split_equation[index-1].to_f - split_equation[index+1].to_f
+    split_equation.slice!(index, 2)
+    split_equation[index-1] = result.to_s
+  end
+end
