6b8bc66c4363dc47b184d12a2b6cc6750fff183f
KojimaNaoyuki <programkoji213@gmail.com>
2023-07-22 12:27:27 +0900

    add unit_config_path

--- lib/unit_manager/initialize_unit.rb
+++ lib/unit_manager/initialize_unit.rb
@@ -5,21 +5,24 @@ module UnitManager
 
     attr_reader :units
 
-    GEM_INSTALL_PATH = __dir__.slice!(0, __dir__.size-4)
-
     def initialize
-      unit_config_path_file = File.open("#{GEM_INSTALL_PATH}/config/unit_config_path.txt", 'r')
-      unit_config_path = unit_config_path_file.read
-      unit_config_path_file.close
-      puts unit_config_path_file
-
-      units_hash = open("#{ENV['unit_config_path']}", 'r') { |f| YAML.load(f) }
+      units_hash = open(unit_config_path, 'r') { |f| YAML.load(f) }
 
       units = units_hash['units'].each_with_object({}) do |unit, units|
         units["#{unit['key']}"] = UnitManager::Unit.new(key: unit['key'], name: unit['name'], equation: unit['equation'])
       end
 
       @units = units
+    end
+
+    def unit_config_path
+      gem_install_path = __dir__.slice!(0, __dir__.size-4)
+
+      unit_config_path_file = File.open("#{gem_install_path}/config/unit_config_path.txt", 'r')
+      unit_config_path = unit_config_path_file.read
+      unit_config_path_file.close
+
+      unit_config_path
     end
   end
 end
