30f0002a770e1c815ffc08ffa2777d7daf081e8a
KojimaNaoyuki <programkoji213@gmail.com>
2023-07-22 10:57:36 +0900

    create config

--- exe/unit.rb
+++ exe/unit.rb
@@ -7,8 +7,10 @@ end
 def init
   Dir.mkdir('unit_config') unless Dir.exist?('unit_config')
   File.new('unit_config/unit.yml', 'w') unless File.exist?("unit_config/unit.yml")
-  
-  ENV['unit_config_path'] = "#{Dir.pwd}/unit_config/unit.yml"
+
+  File.open('../config/unit_config_path.txt', mode = 'w') do | f |
+    f.write("#{Dir.pwd}/unit_config/unit.yml")
+  end
   
   puts '=== Initialize Exit ==='
   puts '- unit_config/unit.yml'
